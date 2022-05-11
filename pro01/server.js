5701e36d9754303b0b3c4ae5cc457ffa00b14533
minh <rnrnck78@naver.com>
2022-05-11 15:31:11 +0900

    node login compelete

--- pro01/server.js
+++ pro01/server.js
@@ -5,62 +5,167 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended : true}));
 
 const MongoClient = require('mongodb').MongoClient;
-const MongoURl = 'mongodb+srv://admin:1234@cluster0.37ayx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 
 app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public'));
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+const dotenv = require('dotenv').config();
+
+
 let db;
-MongoClient.connect(MongoURl, function(error, client){
+MongoClient.connect(process.env.MongoURl, function(error, client){
 
     if(error){
         return console.log(error);
     }
 
     db = client.db('todoapp');
-    // db.collection('post').insertOne({_id:100, title:'test1', content:'test2'},function(err, res){
+    // db.collection('post').insertOne({_id:100, title:'test1', content:'test2'},function(error, result){
     //     console.log('저장완료');
     // });
 
-    app.listen(8080, function(){
+    app.listen(process.env.PORT, function(){
         console.log('listening on 8080');
     });
 });
 
-app.get('/beauty', function(res, resp){
-    resp.send('뷰티용품 사세요');
+app.get('/beauty', function(request, response){
+    response.send('뷰티용품 사세요');
 });
 
-app.get('/',function(res, resp){
-    resp.sendFile(__dirname + '/index.html');
+app.get('/',function(request, response){
+    // response.sendFile(__dirname + '/index.ejs');
+    response.render('index.ejs');
 });
 
-app.get('/write',function(res, resp){
-    resp.sendFile(__dirname + '/write.html');
+app.get('/write',function(request, response){
+    response.render('write.ejs')
 });
 
-app.post('/add', function(res, resp){
-    resp.send('전송완료');
+app.post('/add', function(request, response){
+    response.send('전송완료');
     
-    db.collection('counter').findOne({name : '게시물개수'}, function(err, result){
+    db.collection('counter').findOne({name : '게시물개수'}, function(error, result){
         console.log(result.totalPost);
         let count = result.totalPost;
 
-        db.collection('post').insertOne({_id : count+1, title : res.body.title , content : res.body.content},
-            function(err, res){
-                db.collection('counter').updateOne({name : '게시물개수'},{$inc :{totalPost : 1}},function(err, result){
-                    if(err){
-                        return console.log(err);
+        db.collection('post').insertOne({_id : count+1, title : request.body.title , content : request.body.content},
+            function(error, result){
+                db.collection('counter').updateOne({name : '게시물개수'},{$inc :{totalPost : 1}},function(error, result){
+                    if(error){
+                        return console.log(error);
                     }
                 });
             });
     });
 });
 
-app.get('/list',function(res, resp){
-    db.collection('post').find().toArray(function(err, result){
+app.get('/list',function(request, response){
+    db.collection('post').find().toArray(function(error, result){
         console.log(result);
-        resp.render('list.ejs', {posts : result});
+        response.render('list.ejs', {posts : result});
     });
 
 });
 
+app.delete('/delete', function(request, response){
+    console.log(request.body);
+    request.body._id = parseInt(request.body._id);
+    db.collection('post').deleteOne( request.body, function(error, result){
+        console.log('삭제완료');
+        response.status(200).send({message : '성공했습니다.'});
+    });
+});
+
+app.get('/detail/:id',function(request,response){
+    db.collection('post').findOne({_id : parseInt(request.params.id)}, function(error, result){
+        console.log(result);
+        response.render('detail.ejs', { data : result});
+    })
+});
+
+app.get('/edit/:id', function(request, response){
+    db.collection('post').findOne({_id : parseInt(request.params.id)}, function(error, result){
+        console.log(result);
+        response.render('edit.ejs', {data : result});
+    });
+});
+
+app.put('/edit',function(request, response){
+    db.collection('post').updateOne({_id : parseInt(request.body.id)},
+    { $set :{title : request.body.title, content : request.body.content}}, function(error, result){
+            console.log('수정완료');
+            response.redirect('/list');
+        }
+    );
+});
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized : false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(request, response){
+    response.render('login.ejs');
+});
+
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}),
+    function(request, response){
+        response.redirect('/');
+});
+
+app.get('/mypage', checkLogin, function(request, response){
+    console.log(request.user);
+    response.render('mypage.ejs', {user : request.user});
+});
+
+function checkLogin(request, response, next){
+    if(request.user){
+        next();
+    }else{
+        response.send('로그인 안함');
+    }
+}
+
+passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false
+}, function(입력한아이디, 입력한비번, done){
+    console.log(입력한아이디, 입력한비번);
+    db.collection('login').findOne({id : 입력한아이디}, function(error, result){
+        console.log(result);
+        if(error){
+            return done(error);
+        }
+        if(!result){
+            //done(서버에러, 결과, 에러메세지);
+            return done(null, false, {message : '존재하지않는 아이디입니다.'});
+        }
+
+        if(입력한비번 == result.pw){
+            return done(null, result);
+        }else{
+            return done(null, false, {message : '비밀번호가 틀렸습니다.'});
+        }
+    });
+}));
+
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(아이디, done){
+    db.collection('login').findOne({id : 아이디}, function(error, result){
+        done(null, result);
+    });
+});
